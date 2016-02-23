70c6ee241ce2ed9265d4df9c9a5d9b33a258264f
陈峰 <664839088@qq.com>
2016-02-23 08:16:27 +0800

    Initial Commit

---
+++ ConsultItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ConsultItem
+//
+//  Created by 陈峰 on 16/2/23.
+//  Copyright © 2016年 陈峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ConsultItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ConsultItem
+//
+//  Created by 陈峰 on 16/2/23.
+//  Copyright © 2016年 陈峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
